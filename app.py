f9bb90c16d0f014a477c00cfaa18099e5a7ed0a1
ChristopherNguon <23444@wc.school.nz>
2026-09-23 09:35:33 +1200

    did database steps from 0-8

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
-
+from flask import Flask,render_template
+import sqlite3
+from sqlite3 import Error
 app = Flask(__name__)
-
+DATABASE = 'Character.db'
+def create_connection(db_file):
+   try:
+       connection = sqlite3.connect(db_file)
+       return connection
+   except Error as e:
+       print(e)
+   return None
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/characters')
+def render_characters():
+    query = "SELECT Character,Path,Element,Version,Home_Planet,First_Met,HP,ATK,DEF,SPD FROM Character"
+    con = create_connection(DATABASE)  # runs the function create_connection and passes the bd stared in DATABASE for it to use.
+    cur = con.cursor()  # Applies and stores the cursor system to the data.
+    cur.execute(query)  # runs the query
+    character_list = cur.fetchall()  # stores all the info from the query into the tag list.
+    con.close()
+    print(character_list)
+
+
+@app.route('/styles')
+def render_styles():
+   return render_template("styles.html")
+
 
 
 if __name__ == '__main__':
