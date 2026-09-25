286a6d8d41726604be1acff59b9194e252d0c2e5
ChristopherNguon <23444@wc.school.nz>
2026-09-25 14:15:32 +1200

    25/9/26. dyanmic table

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 import sqlite3
 from sqlite3 import Error
 app = Flask(__name__)
-DATABASE = 'Character.db'
+DATABASE = 'hsr.db'
 def create_connection(db_file):
    try:
        connection = sqlite3.connect(db_file)
@@ -17,18 +17,20 @@ def render_home():  # put application's code here
 
 @app.route('/characters')
 def render_characters():
-    query = "SELECT Character,Path,Element,Version,Home_Planet,First_Met,HP,ATK,DEF,SPD FROM Character"
+    query = "SELECT * FROM Character"
     con = create_connection(DATABASE)  # runs the function create_connection and passes the bd stared in DATABASE for it to use.
     cur = con.cursor()  # Applies and stores the cursor system to the data.
     cur.execute(query)  # runs the query
     character_list = cur.fetchall()  # stores all the info from the query into the tag list.
     con.close()
     print(character_list)
+    return render_template('characters.html', character_list = character_list)
 
 
 @app.route('/styles')
 def render_styles():
    return render_template("styles.html")
+
 
 
 
